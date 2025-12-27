c6bd77049dda012fb9a032affadac49cff5e7b0d
Constantin Buruiana <constbur@gmail.com>
2025-12-26 20:27:38 -0500

    updated dotfiles

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function(args)
+    local file = vim.fs.normalize(args.file)
+    local dir = vim.fs.dirname(file)
+    if not vim.loop.fs_stat(dir) then
+      vim.fn.mkdir(dir, "p")
+    end
+  end,
+})
